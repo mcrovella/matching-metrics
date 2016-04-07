7ed668ae474b16cf9caa21ede83b165574e07b7e
Mark Crovella <crovella@cs.bu.edu>
2016-04-07 14:53:10 -0400

    .

---
+++ basic-ppi-run.sh
@@ -0,0 +1,15 @@
+# Run as of Feb 28, 2016
+
+python run.py 500 0 PPI noperturb 0 human
+python run.py 500 0 PPI noperturb 0 fly
+python run.py 500 0 PPI noperturb 0 mouse
+python run.py 500 0 PPI noperturb 0 worm
+python run.py 500 0 PPI noperturb 0 yeast
+
+python run.py 500 0 PPI scramble 0.25 human
+python run.py 500 0 PPI scramble 0.25 fly
+python run.py 500 0 PPI scramble 0.25 mouse
+python run.py 500 0 PPI scramble 0.25 worm
+python run.py 500 0 PPI scramble 0.25 yeast
+
+

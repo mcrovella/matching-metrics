370e079dd0614b934ec7dfc7a770350d3685b375
Mark Crovella <crovella@cs.bu.edu>
2016-04-07 14:15:20 -0400

    minor

--- basic-samp-run.sh
+++ basic-samp-run.sh
@@ -1,8 +1,12 @@
-# 
+#
+#  These generate the experiments that explore different sampling methods
+#  on PPI-like synthetic graphs
+#
 python sampRun.py 500 0.02 GEO 
 python sampRun.py 500 0.03 GEO 
 python sampRun.py 500 0.06 GEO 
 python sampRun.py 500 0.09 GEO 
 python sampRun.py 500 0.0 SL 
-
+cd samprun/noperturb
+python ../../sampPlots.py
 
